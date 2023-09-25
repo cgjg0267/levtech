fabbdc8013f759506717b9ac5fe97589d79a9d63
cgjg0267 <akriii.7101@gmail.com>
2023-09-26 04:20:07 +0900

    fifth commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPagenateByLimit(1)]);  
+        return view('posts/index')->with(['posts' => $post->getPagenateByLimit(5)]);  
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
@@ -27,6 +27,18 @@ class PostController extends Controller
     {
         $input = $request['post'];
         $post ->fill($input)->save();
+        return redirect('/posts/'. $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post ->fill($input_post)->save();
         return redirect('/posts/'. $post->id);
     }
 }
